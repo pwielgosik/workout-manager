e5a017c83982b80ae3d5c2d519187fd957c3b049
S50B32 <pwielgosik@gmail.com>
2022-08-09 02:09:32 +0200

    Update app component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import WorkoutList from "./components/WorkoutList";
+import Container from "@mui/material/Container";
+import Drawer from "./components/Drawer";
+import AppBar from "./components/AppBar";
+import Preferences from "./components/Preferences";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
+  const [searchInput, setSearchInput] = React.useState("");
+
+  const handleSearchInputChange = (event) => {
+    setSearchInput(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App" maxWidth="false" disableGutters>
+      {/* <AppBar
+            handleSearchInputChange={handleSearchInputChange}
+            searchInputValue={searchInput}
+          /> */}
+      <Drawer appTitle="Workout Manager">
+        <Routes>
+          <Route path="/" element={<h2>Welcome to the Workout Manager</h2>} />
+          <Route path="exercises" element={<h2>Exercises</h2>} />
+          <Route
+            path="workouts"
+            element={<WorkoutList searchInput={searchInput} />}
+          />
+          <Route path="preferences" element={<Preferences />} />
+          <Route path="logout" element={<h2>You have been logged out</h2>} />
+        </Routes>
+      </Drawer>
+    </Container>
   );
 }
 
